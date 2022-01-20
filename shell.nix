0c0f7203425063746e86496169152744f1be2684
Cheng Shao <astrohavoc@gmail.com>
2022-01-20 14:51:47 +0000

    WIP

--- shell.nix
+++ shell.nix
@@ -9,8 +9,10 @@ pkgs.callPackage
       packages = ps: with ps; [ libffi-wasm32 ];
       withHoogle = true;
       nativeBuildInputs = lib.attrValues
-        (import "${sources.hs-nix-tools}/nix/tools.nix" { inherit ghc; })
-      ++ [ clang-tools ] ++ lib.optionals stdenv.isLinux [ util-linux ];
+        (import "${sources.hs-nix-tools}/nix/tools.nix" { inherit ghc; }) ++ [
+        clang-tools
+        (callPackage "${sources.hs-nix-tools}/pkgs/wasmtime" { })
+      ] ++ lib.optionals stdenv.isLinux [ util-linux ];
       exactDeps = true;
       shellHook = lib.optionalString stdenv.isLinux "taskset -pc 0-1000 $$";
     })
