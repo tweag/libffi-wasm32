22feec98bc0d11b67e6f8792fb04506d042cd3fb
Cheng Shao <astrohavoc@gmail.com>
2022-08-10 14:10:51 +0000

    WIP

--- nix/project.nix
+++
@@ -1,5 +0,0 @@
-{ callPackage, ghc, haskell-nix }:
-haskell-nix.cabalProject {
-  src = callPackage ./src.nix { };
-  compiler-nix-name = ghc;
-}
--- nix/src.nix
+++
@@ -1,5 +0,0 @@
-{ haskell-nix }:
-haskell-nix.haskellLib.cleanGit {
-  name = "libffi-wasm32-src";
-  src = ../.;
-}
